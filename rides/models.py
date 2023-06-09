c92217e99903ec87bb3ed07a6c3f79415dd0f0f8
eduardoesr <47727833+eduardoesr@users.noreply.github.com>
2023-06-09 04:19:49 -0300

    Revert "Update models.py"

--- rides/models.py
+++ rides/models.py
@@ -1,28 +1,8 @@
-from django.db import models as mo
-from django.contrib.auth.models import User
+from django.db import models
+
 import datetime
 
 # Create your models here.
-
-
-class profile(mo.Model):
-    class Genero(mo.TextChoices):
-        Opcao_1 = 'Masculino'
-        Opcao_2 = 'Feminino'
-
-    user = mo.OneToOneField(User, on_delete=mo.CASCADE)
-    nome = mo.CharField(max_length=20)
-    idade = mo.IntegerField(max_length=3)
-    email = mo.EmailField(max_length=50)
-    matricula = mo.IntegerField(max_length=20)
-    gender = mo.CharField(
-        max_length=12, choices=Genero.choices, default='Opcao_1')
-
-
-class Rides(mo.Model):
-    User = mo.ForeignKey(User, on_delete=mo.CASCADE)
-    motorista = mo.CharField(max_length=50)
-    passageiro = mo.CharField(max_length=50)
-    data_publicaçao = mo.DateTimeField(datetime.datetime.now)
-    saida = mo.DateTimeField(default=datetime.datetime.now)
-    info = mo.TextField(max_length=200, blank=True)
+class Example(models.Model):
+    text = models.TextField(blank=True)
+    pub_date = models.DateTimeField(default=datetime.datetime.now)
