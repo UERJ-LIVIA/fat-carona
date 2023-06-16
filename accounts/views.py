b80e20d1939bf9c2c8cfa4184d7c79344bf73c6c
Pedro Victor santos de Souza <pedro.souza@discentes.fat.uerj.br>
2023-06-16 19:34:28 -0300

    função CADASTRO modificada para accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate as auth, logout
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+
+def cadastro(request):
+    if request.method == 'POST':
+            try: # verificar se usuario ja existe!
+                 user= User.objects.get (request.POST['username'])
+            except: # se nao existir usuario, cria usuario puxando dados inseridos no frontend !
+                 user=User.obejects.create_user(request.POST['username'],password=request.POST['senha'],email=request.POST['email'])    
+                 auth.login(request,user)
+
+    return  HttpResponseRedirect(request.META.get('login.html'))
