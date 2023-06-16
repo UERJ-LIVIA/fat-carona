ad7366a8fffb60e1799cad833eedb369e6194333
Pedro Victor santos de Souza <pedro.souza@discentes.fat.uerj.br>
2023-06-16 18:35:20 -0300

    alteraçoes relizadas em accounts/models.py e rides/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models as mo
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Profile(mo.Model):
+    class Genero(mo.TextChoices):
+        Opcao_1 = 'Masculino'
+        Opcao_2 = 'Feminino'
+
+    user = mo.OneToOneField(User, on_delete=mo.CASCADE)
+    nome = mo.CharField(max_length=20)
+    idade = mo.IntegerField(max_length=3)
+    email = mo.EmailField(max_length=50)
+    matricula = mo.IntegerField(max_length=20)
+    gender = mo.CharField(
+        max_length=12, choices=Genero.choices, default='Opcao_1')
--- rides/models.py
+++ rides/models.py
@@ -3,6 +3,18 @@ from django.db import models
 import datetime
 
 # Create your models here.
-class Example(models.Model):
-    text = models.TextField(blank=True)
-    pub_date = models.DateTimeField(default=datetime.datetime.now)
+from django.db import models as mo
+import datetime
+from django.contrib.auth.models import User
+from accounts.models import Profile
+
+# Create your models here.
+
+
+class Rides(mo.Model):
+    Profile = mo.ForeignKey(User, on_delete=mo.CASCADE)
+    motorista = mo.ForeignKey(User,on_delete=mo.CASCADE,related_name='driver')
+    passageiro = mo.ManyToManyField(User,related_name='passenger')
+    data_publicaçao = mo.DateTimeField(datetime.datetime.now)
+    saida = mo.DateTimeField(default=datetime.datetime.now)
+    info = mo.TextField(max_length=200, blank=True)
