bed1ea8ab58c2febe0e4ae57dd3c43bfb3518b03
kennethv15 <45489517+kennethv15@users.noreply.github.com>
2023-06-18 19:27:32 -0300

    Update views.py
    feat: Add endpoint for rides with user as driver
    
    Desc: The endpoint receives a POST request with the "departure date", additional information and the user as the driver. From the data, the endpoint creates a ride dictionary with the user as the driver.
    
    Pending: Understand the serializer in order to validate and save the data as a new ride instance in the database.

--- rides/api/views.py
+++ rides/api/views.py
@@ -4,17 +4,39 @@ from rest_framework import generics, mixins
 from ..models import Example
 from .serializers import ExampleSerializer
 
+
 class exampleAPIView(generics.GenericAPIView, 
                     mixins.ListModelMixin,
                     mixins.CreateModelMixin):
-    """
-    Endpoint de exemplo.
-    """
-    queryset            = Example.objects.all()
-    serializer_class    = ExampleSerializer
+
+    queryset = Example.objects.all()
+    serializer_class = ExampleSerializer
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
+        # Convert date to proper format first or nah????
+        departure_date = request.data.get('departure_date')
+
+        # If additional info is inputted by the user, it can be requested from here
+        additional_info = request.data.get('additional_info')
+        user = request.data.get('user')
+
+        # Create ride as a dictionary with the input user as the driver
+        ride_data = {
+            'departure_date': departure_date,
+            'additional_info': additional_info,
+            'driver': user
+        }
+
+        # https://www.django-rest-framework.org/api-guide/generic-views/#examples
+        # https://www.django-rest-framework.org/api-guide/serializers/
+        # https://www.django-rest-framework.org/api-guide/status-codes/
+        #
+        #   serializer = self.get_serializer(data=ride_data)
+        #   self.perform_create(serializer)
+        #
+        #   return Response(serializer.data, status=status.HTTP_201_CREATED)
+
         return self.create(request, *args, **kwargs)
