48a3ed3d0e0058f5181f138e76b66e334943c887
Pedro Victor santos de Souza <pedro.souza@discentes.fat.uerj.br>
2023-09-20 12:07:30 -0300

    alteração em rides/api/urls.py no endpoint ProfilesAPIView

--- Rides/api/urls.py
+++ Rides/api/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
     path('rides/', RidesAPIView.as_view(), name='rides'),
     path('rides/rides-filtro', RidesFiltroAPIView.as_view(), name='rides_filtro'),
     path('rides/<int:pk>/', RideAPIView.as_view(), name='ride'),
-    path('rides/<int:ride_pk>/profiles',
-         ProfilesAPIView.as_view(), name='curso_profiles'),
+    path('profiles/', ProfilesAPIView.as_view(), name='profiles'),
     path('rides/<int:ride_pk>/profiles/<int:profile_pk>',
          ProfileAPIView.as_view(), name='curso_profiles')
 ]
