4562002f12b92a9facc90c429db09958430d77cc
Pedro Victor santos de Souza <pedro.souza@discentes.fat.uerj.br>
2023-09-21 23:19:07 -0300

    alteração na pasta templates

--- Rides/views.py
+++ Rides/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def login (request):
-     return render (request,'login.html')
+
+def login(request):
+    return render(request, 'login.html')
